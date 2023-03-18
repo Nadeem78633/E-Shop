7a90313685a2a981aff565d0918187f765677241
Nadeem78633 <73522426+Nadeem78633@users.noreply.github.com>
2023-03-18 11:14:38 +0530

    final code
    code

---
+++ frontend/build/precache-manifest.63dee377d5e2b59fc5aa8a449d4a5098.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f949d5ae3551bcce197fd8339a9b294c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbee6a424f448b913727",
+    "url": "/static/css/main.2988cb80.chunk.css"
+  },
+  {
+    "revision": "478603ddaccdb6f55609",
+    "url": "/static/js/2.8d545eff.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.8d545eff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbee6a424f448b913727",
+    "url": "/static/js/main.b7cec0bf.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
